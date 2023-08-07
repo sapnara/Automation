6ca3526a9d9667d5f73d8a1888403d8441c73776
Dell <sapnarana8844@gmail.com>
2023-08-07 16:11:02 +0530

    RMS-123 This commit is for changing the user display message

---
+++ src/main/java/org/example/Helloworld.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("This is my first github commit integrating with jenkinss ");
+    }
+}
