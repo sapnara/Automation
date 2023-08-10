230c3e09b3e972c5c80f26076a8bf2b87cb1e5af
sapnara <139849664+sapnara@users.noreply.github.com>
2023-08-10 16:06:31 +0530

    Update Helloworld.java

--- src/main/java/org/example/Helloworld.java
+++ src/main/java/org/example/Helloworld.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("This is my first github commit integrating with jenkinsss ");
+        System.out.println("This is my first github commit integrating with jenkins");
         System.out.println("This demo is for git rebase from Intellij22");
     }
 }
