d39ad90256d0de9b028b32374623199666265f6f
sapnara <139849664+sapnara@users.noreply.github.com>
2023-08-10 16:38:03 +0530

    Demo for broken pipeline

--- src/main/java/org/example/Helloworld.java
+++ src/main/java/org/example/Helloworld.java
@@ -3,6 +3,6 @@ package org.example;
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with jenkins");
-        System.out.println("This demo is for git rebase from Intellij22");
+        System.out.printtln("This demo is for git rebase from Intellij22");
     }
 }
